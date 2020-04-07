22335c3ba8e9c400c04c7f7a5861188edd40b826
Sandie Nunez <snunez@vt.edu>
2020-04-07 01:00:47 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,7 @@
 def starts_with_a_vowel?(word)
-
+  word.match(/^[aeiouAEIOU]\w+/)
+  #word.scan(/\b[AEIOUaeiou][a-z]*\b/)
+  #word.match(/[^aeiou$]\) 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
